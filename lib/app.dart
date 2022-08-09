4687d316a72b1d965d6cba517e4b75dced1464b4
Jean Vallon <jvallon44@gmail.com>
2022-08-08 21:52:35 -0400

    UI of home screen is over

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-
-/// Default value for all paddings in this screen
-const double defaultPadding = 20;
-
-/// Size of titles
-const double h1 = 30;
-const double h2 = 20;
-const double h3 = 15;
+import 'package:widgetbook_challenge/ui/home_page.dart';
 
 /// The app.
 class App extends StatelessWidget {
@@ -18,44 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Interview Challenge'),
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(defaultPadding),
-          child: Column(
-            children: [
-              const Text(
-                'Hello Flutter enthusiast !',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: h2),
-              ),
-              const SizedBox(height: defaultPadding),
-              const Text(
-                'Are you ready to get out of the matrix ? '
-                'Take your time to make your decision, '
-                'because there is no coming back. '
-                '\n\nWhen you are ready, please tap on the Start button.',
-                textAlign: TextAlign.start,
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text('Yay! A SnackBar!'),
-                      ));
-                    },
-                    child: const Text('Start'),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+/// Default value for all paddings in this screen
+const double defaultPadding = 20;
+
+/// Size of titles
+const double h1 = 30;
+const double h2 = 20;
+const double h3 = 15;
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Interview Challenge'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Hello Flutter enthusiast !',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: h2),
+            ),
+            Container(
+              padding: const EdgeInsets.all(defaultPadding),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                    Radius.circular(defaultPadding / 2)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: const Text(
+                'Are you ready to get out of the matrix ? '
+                    'Take your time to make your decision, '
+                    'because there is no coming back. '
+                    '\n\nWhen you are ready, please tap on the Start button.',
+                textAlign: TextAlign.start,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('I love you ❤️'),
+                  ),
+                );
+              },
+              child: const Text('Start'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
